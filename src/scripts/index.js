8e4d2495fb52938231c45d8ae29797310f22e6ba
Lucy Oliphant <lucy.oliphant@the7bridges.com>
2023-11-12 21:31:41 +0000

    Add basic scroll effects

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,7 @@
 import './modules/hide-on-scroll.js';
 
-// import { gsap } from "../../dist/scripts/modules/gsap.min.js";
-// import { ScrollTrigger } from "../../dist/scripts/modules/ScrollTrigger.min.js";
+import { gsap } from "../../dist/scripts/modules/gsap.min.js";
+import { ScrollTrigger } from "../../dist/scripts/modules/ScrollTrigger.min.js";
 
 import LocomotiveScroll from 'locomotive-scroll';
 
@@ -10,6 +10,39 @@ const scroller = new LocomotiveScroll({
   smooth: true
 })
 
+gsap.registerPlugin(ScrollTrigger)
+
+scroller.on('scroll', ScrollTrigger.update)
+
+ScrollTrigger.scrollerProxy(
+  '.main-wrapper', {
+      scrollTop(value) {
+          return arguments.length ?
+          scroller.scrollTo(value, 0, 0) :
+          scroller.scroll.instance.scroll.y
+      },
+      getBoundingClientRect() {
+          return {
+              left: 0, top: 0, 
+              width: window.innerWidth,
+              height: window.innerHeight
+          }
+      }
+  }
+)
 
 
+// ScrollTrigger.create({
+//   trigger: '.image-mask',
+//   scroller: '.main-wrapper',
+//   start: 'top+=30% 50%',
+//   end: 'bottom-=40% 50%',
+//   // animation: gsap.to('.image-mask', {backgroundSize: '120%'}),
+//   scrub: 2,
+//   // markers: true
+// })
 
+
+ScrollTrigger.addEventListener('refresh', () => scroller.update())
+
+ScrollTrigger.refresh()
