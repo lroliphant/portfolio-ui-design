c48cab9117a3a161b569382307262e2c40e45863
Lucy Oliphant <lucy.oliphant@the7bridges.com>
2023-11-24 21:08:54 +0000

    config changes and depdendency updates to remove 11ty/images and add vimeo embed

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,47 +1,36 @@
 const lightningCSS = require("@11tyrocks/eleventy-plugin-lightningcss");
-
 const esbuild = require('esbuild');
 
 module.exports = (config) => {
+
   // to recognize our file and include in the build directory
-  config.addPassthroughCopy("src/images/**/*");
+  config.addPassthroughCopy("src/images/**/**/*");
   config.addPassthroughCopy("src/fonts/*");
   config.addPassthroughCopy("src/styles/**/*");
-	// config.addPassthroughCopy({
-	// 	"./node_modules/gsap/dist/gsap.min.js": "/scripts/modules/gsap.min.js"
-	// });
-	// config.addPassthroughCopy({
-	// 	"./node_modules/gsap/dist/ScrollTrigger.min.js": "/scripts/modules/ScrollTrigger.min.js"
-	// });
-	config.addPassthroughCopy({
-		"./node_modules/locomotive-scroll/dist/locomotive-scroll.min.js": "/scripts/modules/locomotive-scroll.min.js"
-	});
-	// config.addPassthroughCopy({
-	// 	"./node_modules/locomotive-scroll/dist/locomotive-scroll.min.css": "/styles/modules/locomotive-scroll.min.css"
-	// });
+  config.addPassthroughCopy("src/content/**/*");
 
   // watch for changes
   config.addWatchTarget("/src/scripts/");
   config.addWatchTarget("/src/styles/");
 
-  config.addLayoutAlias("base", "layouts/base.njk");
-
-	// lightening css 11ty plugin
+	// plugins
 	config.addPlugin(lightningCSS);
 
-  // JavaScript
-	config.addTemplateFormats('js');
+  // config.addTransform("minifyHtml", require("/src/scripts/transforms/minifyHtml"));
 
-	config.addExtension('js', {
-		outputFileExtension: 'js',
+  // JavaScript
+	config.addTemplateFormats("js");
+
+	config.addExtension("js", {
+		outputFileExtension: "js",
 		compile: async (content, path) => {
-			if (path !== './src/scripts/index.js') {
+			if (path !== "./src/scripts/index.js") {
 				return;
 			}
 
 			return async () => {
 				let { outputFiles } = await esbuild.build({
-					target: 'es2020',
+					target: "es2020",
 					entryPoints: [path],
 					minify: true,
 					bundle: true,
